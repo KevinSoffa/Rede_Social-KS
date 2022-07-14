0141bdef5fd4741c77cbca78e734ef3e26e7b8d9
Kevin Soffa <84532185+KevinSoffa@users.noreply.github.com>
2022-07-13 23:35:07 -0300

    implementando logica no formulario de login e criação

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from forms import FormCriarConta, FormLogin
 from flask import (
     Flask, 
     render_template,
-    url_for
+    request,
+    flash,
+    redirect,
+    url_for,
 )
 
 
@@ -35,10 +38,19 @@ def usuarios():
     )
 
 
-@app.route('/login_criacao')
+# Criação de Conta e Login
+@app.route('/login_criacao', methods=['GET', 'POST'])
 def login_criacao():
     form_login = FormLogin()
     form_criar_conta = FormCriarConta()
+
+    if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
+        flash(f'Login feito com SUCESSO no E-mail: {form_login.email.data}')
+        return redirect(url_for('contato'))
+
+    if form_criar_conta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
+        flash(f'Conta criada com SUCESSO! {form_criar_conta.email.data}')
+        return redirect(url_for('home'))
 
     return render_template(
         'login_criar.html',
